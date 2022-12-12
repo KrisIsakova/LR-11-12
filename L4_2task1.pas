dadcea890d760d90e165d0b28ef0adf8a0909a4a
KrisIsakova <118714648+KrisIsakova@users.noreply.github.com>
2022-12-12 16:25:17 +0300

    Add files via upload

---
+++ L4_2task1.pas
@@ -0,0 +1,20 @@
+﻿uses GraphABC;
+begin
+  SetWindowWidth(1000); 
+  SetWindowHeight(1000);
+  SetPenColor(clblue);
+  moveto(300,200);
+  lineto(400,250);
+  lineto(300,300);
+  lineto(200,250);
+  Lineto(300,200);
+  Moveto(400,250); {линия}
+  Lineto(200,250);
+  Circle(450,250,50);
+  Circle(150,250,50);
+  FloodFill(150,250,clred);{закрасил левый круг}
+  FloodFill(450,250,clyellow); {закрасил правый круг}
+  floodfill(300,225,clblue);
+  floodfill(300,270,clgreen);
+  
+end.
---
+++ L4_2task5.pas
@@ -0,0 +1,22 @@
+﻿uses GraphABC;
+Begin
+Circle(50,50,10);
+   FloodFill(50,50,clblue);
+   Circle(100,100,20);
+   FloodFill(100,100,clpurple);
+   Circle(150,150,30);
+   FloodFill(150,150,clskyblue);
+   Circle(200,200,40);
+   FloodFill(200,200,clfuchsia);
+   Circle(250,250,50);
+   FloodFill(250,250,clpink);
+   Circle(300,300,60);
+   FloodFill(300,300,clnavy);
+   Circle(350,350,70);
+   FloodFill(350,350,clpurple);
+   Circle(400,400,80);
+   FloodFill(400,400,clgreen);
+
+
+
+end.
